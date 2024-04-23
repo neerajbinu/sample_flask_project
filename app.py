bbd37f924b05b6153b2c53571156e7767c0940eb
neerajbinu <neeraj.csb2024@saintgits.org>
2024-04-23 21:53:22 +0530

    Created a file home.html which contains html document and two direectoiries static and dynamic

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/") #when the URL slash (root URL) is accessed, show Hello World
 def hello():
-    return "<p> Hello!World</p>"
+    return render_template("home.html")
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
 
